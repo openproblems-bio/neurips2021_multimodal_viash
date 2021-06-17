20f8ed739b8fb3f6663274725050abd4a44c10a1
Robrecht Cannoodt <rcannood@gmail.com>
2021-06-17 12:47:59 +0200

    store dyngen metadata only if need be

--- src/common/generate_dyngen/script.R
+++ src/common/generate_dyngen/script.R
@@ -9,11 +9,15 @@ par <- list(
   plot = "plot.pdf",
   ssa_tau = 30 / 3600,
   census_interval = 1,
-  compute_atac = TRUE,
-  compute_rna_velocity = FALSE,
-  store_protein = TRUE
+  store_atac = TRUE,
+  store_rna_velocity = FALSE,
+  store_antibody = TRUE
 )
 ## VIASH END
+
+if (par$store_antibody == par$store_atac) {
+  cat("Warning: Strictly pass one of --store_antibody and --store_atac, not neither or both.")
+}
 
 options(tidyverse.quiet = TRUE)
 library(tidyverse)
@@ -42,8 +46,8 @@ init_model <- initialise_model(
     experiment_params = simulation_type_wild_type(
       num_simulations = par$num_simulations
     ),
-    compute_cellwise_grn = par$compute_atac,
-    compute_rna_velocity = par$compute_rna_velocity
+    compute_cellwise_grn = par$store_atac,
+    compute_rna_velocity = par$store_rna_velocity
   ),
   num_cores = par$num_threads,
   verbose = TRUE
@@ -56,75 +60,74 @@ out <- generate_dataset(
   make_plots = TRUE
 )
 
-# get mrna counts
-counts <- out$dataset$X
-
-# construct Ab-like data from protein counts
-# TODO: use real Ab data to map distributions
-counts_ab <- out$dataset$layers[["counts_protein"]]
-
-# sample 50 genes
-sample_genes <-
-  if (ncol(counts_ab) > 50) {
-    sample.int(ncol(counts_ab), 50)
-  } else {
-    seq_len(ncol(counts_ab))
-  }
-
-counts_ab[,-sample_genes] <- 0
-
-counts_ab <- Matrix::drop0(counts_ab)
-
-# constuct atac-like data from single cell regulatory network
-# TODO: use real atac data to map distributions
-regulatory_network <- out$dataset$uns[["regulatory_network"]]
-regulatory_network_sc <- out$dataset$obsm[["regulatory_network_sc"]]
-
-library(Matrix, quietly = TRUE)
-summ <- summary(regulatory_network_sc)
-regnet_ix <- tibble(
-  cell_i = summ$i,
-  reg_i = summ$j,
-  reg_value = summ$x
-) %>%
-  left_join(
-    regulatory_network %>%
-      transmute(
-        reg_i = row_number(),
-        gene_i = match(target, colnames(counts))
-      ),
-    by = "reg_i"
-  )
-
-atac_ix <-
-  regnet_ix %>%
-  group_by(cell_i, gene_i) %>%
-  summarise(
-    atac = sum(reg_value),
-    .groups = "drop"
-  )
-atac <- Matrix::sparseMatrix(
-  i = atac_ix$cell_i,
-  j = atac_ix$gene_i,
-  x = pmax(atac_ix$atac, 0),
-  dims = dim(counts),
-  dimnames = dimnames(counts)
-)
-
+# create initial dataset
 adata <- anndata::AnnData(
   X = NULL,
   obs = out$dataset$obs,
   var = out$dataset$var,
   shape = dim(counts),
   layers = list(
-    mrna = counts,
-    antibody = counts_ab,
-    atac = atac
+    mrna = out$dataset$X
   ),
   uns = list(
-    dataset_id = par$dataset_id
+    dataset_id = par$id
   )
 )
+
+if (par$store_antibody) {
+  # construct Ab-like data from protein counts
+  # TODO: use real Ab data to map distributions
+  counts_ab <- out$dataset$layers[["counts_protein"]]
+
+  # sample 50 genes
+  if (ncol(counts_ab) > 50) {
+    sample_genes <- sample.int(ncol(counts_ab), 50)
+    counts_ab[,-sample_genes] <- 0
+    counts_ab <- Matrix::drop0(counts_ab)
+  }
+
+  adata$layers[["antibody"]] <- counts_ab
+}
+
+if (par$store_atac) {
+  # constuct atac-like data from single cell regulatory network
+  # TODO: use real atac data to map distributions
+  regulatory_network <- out$dataset$uns[["regulatory_network"]]
+  regulatory_network_sc <- out$dataset$obsm[["regulatory_network_sc"]]
+
+  library(Matrix, quietly = TRUE)
+  summ <- summary(regulatory_network_sc)
+  regnet_ix <- tibble(
+    cell_i = summ$i,
+    reg_i = summ$j,
+    reg_value = summ$x
+  ) %>%
+    left_join(
+      regulatory_network %>%
+        transmute(
+          reg_i = row_number(),
+          gene_i = match(target, colnames(counts))
+        ),
+      by = "reg_i"
+    )
+
+  atac_ix <-
+    regnet_ix %>%
+    group_by(cell_i, gene_i) %>%
+    summarise(
+      atac = sum(reg_value),
+      .groups = "drop"
+    )
+  atac <- Matrix::sparseMatrix(
+    i = atac_ix$cell_i,
+    j = atac_ix$gene_i,
+    x = pmax(atac_ix$atac, 0),
+    dims = dim(counts),
+    dimnames = dimnames(counts)
+  )
+
+  adata$layers[["atac"]] <- atac
+}
 
 adata$write_h5ad(par$output, compression = "gzip")
 
--- src/common/generate_dyngen/test.R
+++ src/common/generate_dyngen/test.R
@@ -10,25 +10,24 @@ out <- processx::run(
     "--id", "mytest",
     "--backbone", "bifurcating",
     "--output", "dataset.h5ad",
-    "--output_censored", "dataset_censored.h5ad",
     "--plot", "plot.pdf",
-    "--model", "model.rds",
     "--num_threads", "1",
     "--num_simulations", "3",
     "--num_cells", "100",
     "--num_genes", "50",
     "--census_interval", "2",
-    "--compute_rna_velocity",
-    "--compute_cellwise_grn"
+    "--store_rna_velocity",
+    "--store_atac",
+    "--store_antibody"
   ),
   stderr_to_stdout = TRUE
 )
 
 cat("> Checking whether output files were created\n")
-assert_that(file.exists("dataset.h5ad"))
-assert_that(file.exists("dataset_censored.h5ad"))
-assert_that(file.exists("plot.pdf"))
-assert_that(file.exists("model.rds"))
+assert_that(
+  file.exists("dataset.h5ad"),
+  file.exists("plot.pdf")
+)
 
 cat("> Checking contents of dataset.h5ad\n")
 adata <- anndata::read_h5ad("dataset.h5ad")
@@ -37,20 +36,7 @@ assert_that(
   adata$uns[["dataset_id"]] == "mytest",
   adata$n_obs == 100,
   adata$n_vars == 50,
-  "regulatory_network_sc" %in% adata$obsm_keys(),
-  all(c("counts_spliced", "counts_unspliced", "counts_protein") %in% names(adata$layers))
-)
-
-
-cat("> Checking contents of dataset_censored.h5ad\n")
-adata2 <- anndata::read_h5ad("dataset_censored.h5ad")
-
-assert_that(
-  adata2$uns[["dataset_id"]] == "mytest",
-  adata2$n_obs == 100,
-  adata2$n_vars == 50,
-  all(c("modality1", "modality2") %in% names(adata2$layers)),
-  all(names(adata2$layers) %in% c("modality1", "modality2"))
+  all(c("mrna", "antibody", "atac") %in% names(adata$layers))
 )
 
 cat("> Test succeeded!\n")
