e6e029511a8e4deef2e163208ee002215787788e
Robrecht Cannoodt <rcannood@gmail.com>
2021-07-06 20:55:14 +0200

    fix 10x test

--- src/common/datasets/download_10x_dataset/run_test.R
+++ src/common/datasets/download_10x_dataset/run_test.R
@@ -8,7 +8,8 @@ cat(">> Testing with citeseq dataset\n")
 system(paste0(
   "./download_10x_dataset ",
   "--input https://cf.10xgenomics.com/samples/cell-exp/3.0.0/pbmc_1k_protein_v3/pbmc_1k_protein_v3_raw_feature_bc_matrix.h5 ",
-  "--output output1.h5ad "
+  "--output output1.h5ad ",
+  "--id dataset1"
 ))
 
 expect_true(file.exists("output1.h5ad"), info = "Output output1.h5ad was not created")
@@ -28,7 +29,8 @@ cat(">> Testing with atacseq dataset\n")
 system(paste0(
   "./download_10x_dataset ",
   "--input https://cf.10xgenomics.com/samples/cell-arc/2.0.0/lymph_node_lymphoma_14k/lymph_node_lymphoma_14k_raw_feature_bc_matrix.h5 ",
-  "--output output2.h5ad "
+  "--output output2.h5ad ",
+  "--id dataset2"
 ))
 
 expect_true(file.exists("output2.h5ad"), info = "Output output2.h5ad was not created")
