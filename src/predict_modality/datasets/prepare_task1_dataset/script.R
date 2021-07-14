26986c84aea0a52d4d57f9887d41b81ddb48903f
Robrecht Cannoodt <rcannood@gmail.com>
2021-07-14 16:10:04 +0200

    add max mod2 columns parameter to task1

--- src/predict_modality/datasets/prepare_task1_dataset/script.R
+++ src/predict_modality/datasets/prepare_task1_dataset/script.R
@@ -7,24 +7,23 @@ library(Matrix, quietly = TRUE, warn.conflicts = FALSE)
 
 ## VIASH START
 par <- list(
-  input_mod1 = "output/common_datasets/dyngen_atac_small/dyngen_atac_small.normalize.output_rna.h5ad",
-  input_mod2 = "output/common_datasets/dyngen_atac_small/dyngen_atac_small.normalize.output_mod2.h5ad",
+  input_mod1 = "resources_test/common/pbmc_1k_protein_v3.normalize.output_rna.h5ad",
+  input_mod2 = "resources_test/common/pbmc_1k_protein_v3.normalize.output_mod2.h5ad",
   output_mod1 = "output_mod1.h5ad",
   output_mod2 = "output_mod2.h5ad",
   output_solution = "solution.h5ad",
-  # input_rna = "resources_test/common/pbmc_1k_protein_v3.normalize.output_rna.h5ad",
-  # input_mod2 = "resources_test/common/pbmc_1k_protein_v3.normalize.output_mod2.h5ad",
-  # output_mod1 = "resources_test/task1/pbmc_1k_protein_v3.output_mod1.h5ad",
-  # output_mod2 = "resources_test/task1/pbmc_1k_protein_v3.output_mod2.h5ad",
-  # output_solution = "resources_test/task1/pbmc_1k_protein_v3.solution.h5ad",
-  mod1 = "RNA",
-  seed = 1L
+  seed = 1L,
+  max_mod2_columns = 5000L
 )
 ## VIASH END
 
 cat("Reading input data\n")
 ad1_raw <- anndata::read_h5ad(par$input_mod1)
 ad2_raw <- anndata::read_h5ad(par$input_mod2)
+ad1_mod <- unique(ad1_raw$var[["feature_types"]])
+ad2_mod <- unique(ad2_raw$var[["feature_types"]])
+new_dataset_id <- paste0(ad1_raw$uns[["dataset_id"]], "_task1_", tolower(ad1_mod))
+common_uns <- list(dataset_id = new_dataset_id)
 
 cat("Determining train/test split\n")
 split <- 
@@ -44,43 +43,49 @@ ad1_X <- ad1_raw$X[splor,, drop = FALSE]
 ad2_X <- ad2_raw$X[splor,, drop = FALSE]
 split <- split[splor]
 
+if (!is.null(par$max_mod2_columns) && par$max_mod2_columns < ncol(ad2_X)) {
+  cat("Sampling mod2 columns\n")
+  ad2_ix <- sample.int(ncol(ad2_X), par$max_mod2_columns)
+  ad2_X <- ad2_X[, ad2_ix, drop = FALSE]
+}
+
 cat("Creating mod1 object\n")
 out_mod1 <- anndata::AnnData(
   X = ad1_X,
+  var = data.frame(
+    row.names = colnames(ad1_X),
+    feature_types = rep(ad1_mod, ncol(ad1_X))
+  ),
   obs = data.frame(
-    row.names = rownames(ad1_raw),
+    row.names = rownames(ad1_X),
     split = split
   ),
-  uns = list(
-    dataset_id = paste0(ad1_raw$uns[["dataset_id"]], "_task1"),
-    modality = ad1_raw$uns[["modality"]]
-  )
+  uns = common_uns
 )
 
 cat("Creating mod2 object\n")
+ad2_var <- data.frame(
+  row.names = colnames(ad2_X),
+  feature_types = rep(ad2_mod, ncol(ad2_X))
+)
+ad2_obs <- data.frame(
+  row.names = rownames(ad2_X),
+  split = split
+)
+
 out_mod2 <- anndata::AnnData(
   X = ad2_X[split == "train",, drop = FALSE],
-  obs = data.frame(
-    row.names = rownames(ad2_raw)[split == "train"],
-    split = split[split == "train"]
-  ),
-  uns = list(
-    dataset_id = paste0(ad2_raw$uns[["dataset_id"]], "_task1"),
-    modality = ad2_raw$uns[["modality"]]
-  )
+  var = ad2_var,
+  obs = ad2_obs[split == "train",, drop = FALSE],
+  uns = common_uns
 )
 
 cat("Create solution object\n")
 out_solution <- anndata::AnnData(
   X = ad2_X[split == "test",, drop = FALSE],
-  obs = data.frame(
-    row.names = rownames(ad2_raw)[split == "test"],
-    split = split[split == "test"]
-  ),
-  uns = list(
-    dataset_id = paste0(ad2_raw$uns[["dataset_id"]], "_task1"),
-    modality = ad2_raw$uns[["modality"]]
-  )
+  var = ad2_var,
+  obs = ad2_obs[split == "test",, drop = FALSE],
+  uns = common_uns
 )
 
 cat("Saving output files as h5ad\n")
