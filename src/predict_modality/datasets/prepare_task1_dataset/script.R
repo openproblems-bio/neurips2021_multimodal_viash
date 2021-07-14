be9aa86626b902477c85d69990e8d4e26d6dc733
Robrecht Cannoodt <rcannood@gmail.com>
2021-07-14 14:07:51 +0200

    rework prep1 component

--- src/predict_modality/datasets/prepare_task1_dataset/script.R
+++ src/predict_modality/datasets/prepare_task1_dataset/script.R
@@ -7,23 +7,24 @@ library(Matrix, quietly = TRUE, warn.conflicts = FALSE)
 
 ## VIASH START
 par <- list(
-  input_rna = "resources_test/common/pbmc_1k_protein_v3.normalize.output_rna.h5ad",
-  input_mod2 = "resources_test/common/pbmc_1k_protein_v3.normalize.output_mod2.h5ad",
-  output_mod1 = "resources_test/task1/pbmc_1k_protein_v3.output_mod1.h5ad",
-  output_mod2 = "resources_test/task1/pbmc_1k_protein_v3.output_mod2.h5ad",
-  output_solution = "resources_test/task1/pbmc_1k_protein_v3.solution.h5ad",
+  input_mod1 = "output/common_datasets/dyngen_atac_small/dyngen_atac_small.normalize.output_rna.h5ad",
+  input_mod2 = "output/common_datasets/dyngen_atac_small/dyngen_atac_small.normalize.output_mod2.h5ad",
+  output_mod1 = "output_mod1.h5ad",
+  output_mod2 = "output_mod2.h5ad",
+  output_solution = "solution.h5ad",
+  # input_rna = "resources_test/common/pbmc_1k_protein_v3.normalize.output_rna.h5ad",
+  # input_mod2 = "resources_test/common/pbmc_1k_protein_v3.normalize.output_mod2.h5ad",
+  # output_mod1 = "resources_test/task1/pbmc_1k_protein_v3.output_mod1.h5ad",
+  # output_mod2 = "resources_test/task1/pbmc_1k_protein_v3.output_mod2.h5ad",
+  # output_solution = "resources_test/task1/pbmc_1k_protein_v3.solution.h5ad",
   mod1 = "RNA",
   seed = 1L
 )
 ## VIASH END
 
 cat("Reading input data\n")
-rna_mod1 <- tolower(par$mod1) == "rna"
-ad1_path <- if (rna_mod1) { par$input_rna } else { par$input_mod2 }
-ad2_path <- if (rna_mod1) { par$input_mod2 } else { par$input_rna }
-
-ad1_raw <- anndata::read_h5ad(ad1_path)
-ad2_raw <- anndata::read_h5ad(ad2_path)
+ad1_raw <- anndata::read_h5ad(par$input_mod1)
+ad2_raw <- anndata::read_h5ad(par$input_mod2)
 
 cat("Determining train/test split\n")
 split <- 
@@ -39,13 +40,13 @@ split <-
     ifelse(seq_len(nrow(ad1_raw)) %in% ix, "train", "test")
   }
 splor <- order(split)
-ad1_raw <- ad1_raw[splor, ]
-ad2_raw <- ad2_raw[splor, ]
+ad1_X <- ad1_raw$X[splor,, drop = FALSE]
+ad2_X <- ad2_raw$X[splor,, drop = FALSE]
 split <- split[splor]
 
 cat("Creating mod1 object\n")
 out_mod1 <- anndata::AnnData(
-  X = ad1_raw$X,
+  X = ad1_X,
   obs = data.frame(
     row.names = rownames(ad1_raw),
     split = split
@@ -58,7 +59,7 @@ out_mod1 <- anndata::AnnData(
 
 cat("Creating mod2 object\n")
 out_mod2 <- anndata::AnnData(
-  X = ad2_raw$X[split == "train", ],
+  X = ad2_X[split == "train",, drop = FALSE],
   obs = data.frame(
     row.names = rownames(ad2_raw)[split == "train"],
     split = split[split == "train"]
@@ -71,7 +72,7 @@ out_mod2 <- anndata::AnnData(
 
 cat("Create solution object\n")
 out_solution <- anndata::AnnData(
-  X = ad2_raw$X[split == "test", ],
+  X = ad2_X[split == "test",, drop = FALSE],
   obs = data.frame(
     row.names = rownames(ad2_raw)[split == "test"],
     split = split[split == "test"]
