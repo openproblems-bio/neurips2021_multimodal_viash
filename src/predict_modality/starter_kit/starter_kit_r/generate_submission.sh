7b4ef37c8b2505fd7d3ee3b609f4f5599c6da850
Robrecht Cannoodt <rcannood@gmail.com>
2021-07-16 09:31:44 +0200

    simplify starter kit

--- src/predict_modality/starter_kit/starter_kit_r/generate_submission.sh
+++ src/predict_modality/starter_kit/starter_kit_r/generate_submission.sh
@@ -3,33 +3,40 @@
 set +x 
 
 [ ! -f config.vsh.yaml ] && echo "Couldn't find 'config.vsh.yaml!" && exit 1
+# todo: add more checks
+# e.g. is nextflow and viash on path?
 
-# building docker container and docker executable
+echo "###########################################"
+echo "## Build docker executable and container ##"
+echo "###########################################"
+echo ""
 viash build config.vsh.yaml -o target/docker -p docker --setup cachedbuild \
   -c '.functionality.name := "method"'
 
-# building nextflow module
+echo "###########################"
+echo "## Build nextflow module ##"
+echo "###########################"
+echo ""
 viash build config.vsh.yaml -o target/nextflow -p nextflow \
   -c '.functionality.name := "method"' \
   -c '.platforms[.type == "nextflow"].publish := true' \
   -c '.platforms[.type == "nextflow"].directive_time := "10m"' \
   -c '.platforms[.type == "nextflow"].directive_memory := "20 GB"'
 
-# to do: set to repository
-# nextflow drop openproblems-bio/neurips2021_multimodal_viash
-# nextflow \
-#   run openproblems-bio/neurips2021_multimodal_viash \
-#   -main-script src/predict_modality/workflows/evaluate_task1_method/main.nf \
-#   --publishDir output/task1_predictions/ \
-#   -resume
-
-# running viash pipeline
-NXF_VER=21.04.1 nextflow \
-  run . \
-  -main-script nextflow/main.nf \
+echo "################################################"
+echo "## Generating submission files using nextflow ##"
+echo "################################################"
+echo ""
+nextflow \
+  run openproblems-bio/neurips2021_multimodal_viash:0.1.0 \
+  -main-script src/predict_modality/workflows/evaluate_task1_method/main.nf \
   --publishDir output/task1_predictions/ \
   -resume
 
+echo "#############################"
+echo "## Creating submission zip ##"
+echo "#############################"
+echo ""
 [ -f submission.zip ] && rm submission.zip
 zip -9 -rv submission.zip . \
   --exclude=*.git* \
@@ -37,14 +44,11 @@ zip -9 -rv submission.zip . \
   --exclude=*work* \
   --exclude=*.DS_Store*
 
+# print message
 echo "Done! Please upload your submission at the link below:"
 echo "  https://eval.ai/web/challenges/challenge-page/1111/submission"
-echo "Alternatively, if you've set up the evalai cli, use the command below create a private submission:"
+echo "If you set up the eval.ai CLI tool, use the command below create a private submission:"
 echo "  evalai challenge 1111 phase 2276 submit --file submission.zip --large --private"
 echo "Or this command to create a public one:"
 echo "  evalai challenge 1111 phase 2276 submit --file submission.zip --large --public"
 echo "Good luck!"
-# check the 'submission instructions' at the bottom of this page to set up eval cli
-# https://eval.ai/web/challenges/challenge-page/1111/submission
-# if you've set up the evalai-cli, you can use this command to make a private submissions
-# evalai challenge 1111 phase 2276 submit --file submission.zip --large --private
