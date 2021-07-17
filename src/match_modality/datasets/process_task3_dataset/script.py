42c9ea237a51e29ea415199d7378d71f95215d6e
Robrecht Cannoodt <rcannood@gmail.com>
2021-07-17 16:00:47 +0200

    fix process task3 script

--- src/match_modality/datasets/process_task3_dataset/script.py
+++ src/match_modality/datasets/process_task3_dataset/script.py
@@ -1,5 +1,4 @@
-# LOAD DEPENDENCIES
-
+print("Loading dependencies")
 import anndata
 import random
 import pandas as pd
@@ -8,63 +7,57 @@ import scipy.sparse
 
 # VIASH START
 par = {
-    "input_rna": "pbmc_1k_protein_v3.output_rna.h5ad",
-    "input_mod2": "pbmc_1k_protein_v3.output_mod2.h5ad",
-    "output_mod1": "pbmc_1k_protein_v3.censored_rna.h5ad",
-    "output_mod2": "pbmc_1k_protein_v3.censored_mod2.h5ad",
-    "output_solution": "pbmc_1k_protein_v3.solution.h5ad",
+    "input_mod1": "resources_test/common/test_resource.output_rna.h5ad",
+    "input_mod2": "resources_test/common/test_resource.output_mod2.h5ad",
+    "output_mod1": "resources_test/task3/test_resource.mod1.h5ad",
+    "output_mod2": "resources_test/task3/test_resource.mod2.h5ad",
+    "output_solution": "resources_test/task3/test_resource.solution.h5ad",
 }
 # VIASH END
 
-# READ INPUT DATA
-rna = anndata.read_h5ad(par["input_rna"])
+print("Reading input data")
+mod1 = anndata.read_h5ad(par["input_mod1"])
 mod2 = anndata.read_h5ad(par["input_mod2"])
 
-# TODO: change on how to know which modality is there
-# modality_types = {"modality1": "rna", "modality2": "chromatin" if has_atac else "protein"}
-
-#  CREATE CENSOR OBJECT
-pairings = scipy.sparse.spdiags(np.full(rna.shape[0], 1), diags=0, m=rna.shape[0], n=mod2.shape[0], format="csr")
+print("Shuffling rows of mod2")
+pairings = scipy.sparse.spdiags(np.full(mod1.shape[0], 1), diags=0, m=mod1.shape[0], n=mod2.shape[0], format="csr")
 
 # Generate the indices partition
-shuffle_cells = list(range(rna.shape[0]))
+shuffle_cells = list(range(mod1.shape[0]))
 random.shuffle(shuffle_cells)
 
-# shuffle according to these indices
-mod2 = mod2[shuffle_cells, :]
-pairings = pairings[:, shuffle_cells]
-
-censor_rna = anndata.AnnData(
-    X=rna.X,
+print("Creating output objects")
+desired_var1_cols = [x for x in ["gene_ids", "feature_types"] if x in mod1.var.columns]
+out_mod1 = anndata.AnnData(
+    X=mod1.X,
+    var=mod1.var[desired_var1_cols],
     uns={
-        "dataset_id": rna.uns["dataset_id"] + "_task3",
-        "modality": "rna",
-        "paired": True,
+        "dataset_id": mod1.uns["dataset_id"] + "_task3"
     },
     dtype="float32",
 )
+out_mod1.X.sort_indices()
 
-censor_mod2 = anndata.AnnData(
-    X=mod2.X,
+desired_var2_cols = [x for x in ["gene_ids", "feature_types"] if x in mod2.var.columns]
+out_mod2 = anndata.AnnData(
+    X=mod2.X[shuffle_cells, :],
+    var=mod2.var[desired_var2_cols],
     uns={
-        "dataset_id": mod2.uns["dataset_id"] + "_task3",
-        "modality": mod2.uns["modality"],
-        "paired": True,
+        "dataset_id": mod2.uns["dataset_id"] + "_task3"
     },
     dtype="float32",
 )
+out_mod2.X.sort_indices()
 
-censor_rna.write_h5ad(filename=par["output_mod1"], compression="gzip")
-censor_mod2.write_h5ad(filename=par["output_mod2"], compression="gzip")
-
-# CREATE SOLUTION PAIRING
 out_solution = anndata.AnnData(
-    shape=rna.shape,
+    X=pairings[:, shuffle_cells],
     uns={
-        "dataset_id": rna.uns["dataset_id"] + "_task3",
-        "pairings": pairings,
+        "dataset_id": mod1.uns["dataset_id"] + "_task3"
     },
     dtype="float32"
 )
 
+print("Writing output objects to file")
+out_mod1.write_h5ad(filename=par["output_mod1"], compression="gzip")
+out_mod2.write_h5ad(filename=par["output_mod2"], compression="gzip")
 out_solution.write_h5ad(filename=par["output_solution"], compression="gzip")
