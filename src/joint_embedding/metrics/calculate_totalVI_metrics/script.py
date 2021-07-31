4ac7a4874d3e55bab781f4da607807a7de1ae5b2
atchen <ann.chen@yale.edu>
2021-07-30 17:22:33 -0700

    Added latent mixing metric

--- src/joint_embedding/metrics/calculate_totalVI_metrics/script.py
+++ src/joint_embedding/metrics/calculate_totalVI_metrics/script.py
@@ -5,8 +5,6 @@ from scvi.inference import TotalPosterior, TotalTrainer, Posterior, Unsupervised
 import os
 import sys
 
-save_path = "/mnt/ibm_lg/achen/neurips/totalvi/"
-
 import anndata
 import scanpy as sc
 import numpy as np
@@ -19,24 +17,20 @@ from utils import *
 par = {
     "input_prediction": "/resources_test/joint_embedding/test_resource.prediction.h5ad",
     "input_solution": "/resources_test/joint_embedding/test_resource.solution.h5ad",
-    "output": "/resources_test/joint_embedding/test_resource.scores_totalvi.h5ad",
+    "output_metrics": "/resources_test/joint_embedding/test_resource.scores_totalvi.h5ad",
 }
 # VIASH END
 
-predict_adata = sc.read_h5ad("input_prediction")
-solution_mod2 = sc.read_h5ad("input_solution")
+predict_adata = sc.read_h5ad(par["input_prediction"])
+solution_adata = sc.read_h5ad(par["input_solution"])
 
 method_name = predict_adata.uns['method_id'] 
 
 metrics_adata = predict_adata
 metrics_adata.obsm[method_name] = predict_adata.X
 
+# calculate mixing statistics
 ENTROPY_K = 100
-harmo_metrics = pd.DataFrame(
-    index=[method_name],
-    columns=["Latent mixing metric", "Feature retention metric", "Clustering metric", 
-             'Measurement mixing metric - gene', 'Measurement mixing metric - protein'],
-
 knn = np.ceil(metrics_adata.n_obs * (np.arange(0.1, 1, 0.1)) / 100).astype(int)
 
 # calculate latent mixing metric
@@ -55,4 +49,4 @@ uns = {"dataset_id": predict_adata.uns["dataset_id"],
 
 adata_out.uns = uns
 
-adata_out.write_h5ad('output_metrics', compression = "gzip")
+adata_out.write_h5ad(par['output_metrics'], compression = "gzip")
