a111a1549722d9fc8d5d8c7ba5e593ab2b82e161
Robrecht Cannoodt <rcannood@gmail.com>
2021-08-03 08:55:41 +0200

    update and extend tests

--- src/joint_embedding/metrics/asw_label/test.py
+++ src/joint_embedding/metrics/asw_label/test.py
@@ -1,12 +1,14 @@
 from os import path
 import subprocess
-import pandas as pd
+import anndata as ad
+# import pandas as pd
 import numpy as np
 
 np.random.seed(42)
 
 metric = 'asw_label'
-metric_file = metric + '.tsv'
+# metric_file = metric + '.tsv'
+metric_file = metric + '.h5ad'
 
 print(">> Running script")
 out = subprocess.check_output([
@@ -18,14 +20,32 @@ out = subprocess.check_output([
 
 print(">> Checking whether file exists")
 assert path.exists(metric_file)
-result = pd.read_table(metric_file)
+# result = pd.read_table(metric_file)
+result = ad.read_h5ad(metric_file)
+sol = ad.read_h5ad('resources_test/joint_embedding/test_resource.solution.h5ad')
+pred = ad.read_h5ad('resources_test/joint_embedding/test_resource.prediction.h5ad')
 
-print(">> Check that score makes sense")
-assert result.shape == (1, 4)
-score = result.loc[0, 'value']
+# print(">> Check that score makes sense")
+# assert result.shape == (1, 4)
+# score = result.loc[0, 'value']
+# print(score)
+
+print(">> Check contents of result.uns")
+assert 'dataset_id' in result.uns
+assert result.uns['dataset_id'] == sol.uns['dataset_id']
+
+assert 'method_id' in result.uns
+assert result.uns['method_id'] == pred.uns['method_id']
+
+assert 'metric_ids' in result.uns
+assert result.uns['metric_ids'] == [metric]
+
+assert 'metric_moreisbetter' in result.uns
+
+assert 'metric_values' in result.uns
+score = result.uns['metric_values'][0]
 print(score)
-
-assert 0 < score < 1
+assert 0 <= score <= 1
 assert score == 0.4662921130657196
 
 print(">> All tests passed successfully")
