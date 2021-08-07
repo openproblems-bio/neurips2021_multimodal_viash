7ab0e842cc4d8385ac7ea7880cd3445f71298f68
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-08-07 15:15:44 +0200

    Added code for creating Python starter kit

--- src/common/create_starter_kits/create_all.sh
+++ src/common/create_starter_kits/create_all.sh
@@ -13,3 +13,11 @@ bin/viash run src/common/create_starter_kits/create_starter_kit/config.vsh.yaml 
   --language r \
   --language_name R \
   --evalai_phase 2276
+
+bin/viash run src/common/create_starter_kits/create_starter_kit/config.vsh.yaml -- \
+  --input_dir src/predict_modality/starter_kit/starter_kit_python/ \
+  --task predict_modality \
+  --task_name "Predict Modality" \
+  --language python \
+  --language_name Python \
+  --evalai_phase 2276
