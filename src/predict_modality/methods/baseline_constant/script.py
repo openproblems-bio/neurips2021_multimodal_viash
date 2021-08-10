d5c7c4b63d8c7132103e2334a98d29c8bf9d91ac
Louise Deconinck <louise.deconinck@gmail.com>
2021-08-10 12:10:39 +0200

    Refactor baseline_constant of task 1

--- src/predict_modality/methods/baseline_constant/script.py
+++ src/predict_modality/methods/baseline_constant/script.py
@@ -5,24 +5,24 @@ import numpy as np
 
 # VIASH START
 par = {
-    "input_mod1": "../../../../resources_test/predict_modality/test_resource.mod1.h5ad",
+    "input_mod1_train": "../../../../resources_test/predict_modality/test_resource.mod1.h5ad",
+    "input_mod1_test": "../../../../resources_test/predict_modality/test_resource.mod1.h5ad",
     "input_mod2": "../../../../resources_test/predict_modality/test_resource.mod2.h5ad",
     "output": "../../../../resources_test/predict_modality/test_resource.prediction.h5ad",
 }
 # VIASH END
 
 # load dataset to be censored
-ad_rna = anndata.read_h5ad(par["input_mod1"])
+ad_rna_train = anndata.read_h5ad(par["input_mod1_train"])
+ad_rna_test = anndata.read_h5ad(par["input_mod1_test"])
 ad_mod2 = anndata.read_h5ad(par["input_mod2"])
 
 
 # Find the correct shape
 
-ad_train = ad_rna[ad_rna.obs["group"] == "train"]
 ad_mod2_train = ad_mod2[ad_mod2.obs["group"] == "train"]
 mean = np.array(ad_mod2_train.X.mean(axis=0)).flatten()
-ad_test = ad_rna[ad_rna.obs["group"] == "test"]
-prediction = np.tile(mean, (ad_test.shape[0], 1))
+prediction = np.tile(mean, (ad_rna_test.shape[0], 1))
 
 # Write out prediction
 out = anndata.AnnData(
