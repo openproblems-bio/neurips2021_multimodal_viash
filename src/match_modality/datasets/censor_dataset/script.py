6f575b2a9df44c62b311887a52b1f9c16a9f6a1c
Robrecht Cannoodt <rcannood@gmail.com>
2021-08-10 13:49:15 +0200

    update match modality censor component

--- src/match_modality/datasets/censor_dataset/script.py
+++ src/match_modality/datasets/censor_dataset/script.py
@@ -1,7 +1,6 @@
 print("Loading dependencies")
-import anndata
+import anndata as ad
 import random
-import pandas as pd
 import numpy as np
 import scipy.sparse
 
@@ -9,50 +8,87 @@ import scipy.sparse
 par = {
     "input_mod1": "resources_test/common/test_resource.output_rna.h5ad",
     "input_mod2": "resources_test/common/test_resource.output_mod2.h5ad",
-    "output_mod1": "resources_test/match_modality/test_resource.mod1.h5ad",
-    "output_mod2": "resources_test/match_modality/test_resource.mod2.h5ad",
-    "output_solution": "resources_test/match_modality/test_resource.solution.h5ad",
+    "output_train_mod1": "resources_test/match_modality/test_resource.train_mod1.h5ad",
+    "output_train_mod2": "resources_test/match_modality/test_resource.train_mod2.h5ad",
+    "output_train_sol": "resources_test/match_modality/test_resource.train_sol.h5ad",
+    "output_test_mod1": "resources_test/match_modality/test_resource.test_mod1.h5ad",
+    "output_test_mod2": "resources_test/match_modality/test_resource.test_mod2.h5ad",
+    "output_test_sol": "resources_test/match_modality/test_resource.test_sol.h5ad"
 }
 # VIASH END
 
 print("Reading input data")
-mod1 = anndata.read_h5ad(par["input_mod1"])
-mod2 = anndata.read_h5ad(par["input_mod2"])
+mod1 = ad.read_h5ad(par["input_mod1"])
+mod2 = ad.read_h5ad(par["input_mod2"])
 new_dataset_id = mod1.uns["dataset_id"] + "_MM"
 
-print("Shuffling rows of mod2")
-pairings = scipy.sparse.spdiags(np.full(mod1.shape[0], 1), diags=0, m=mod1.shape[0], n=mod2.shape[0], format="csr")
+print("Shuffling train cells")
+train_mod1_ix = np.where(mod1.obs["is_train"])[0]
+train_mod2_ix = train_mod1_ix.copy()
+random.shuffle(train_mod2_ix)
+train_mod1 = mod1[train_mod1_ix, :]
+train_mod2 = mod2[train_mod2_ix, :]
 
-# Generate the indices partition
-shuffle_cells = list(range(mod1.shape[0]))
-random.shuffle(shuffle_cells)
+print("Shuffling test cells")
+test_mod1_ix = np.where(~mod1.obs["is_train"])[0]
+test_mod2_ix = test_mod1_ix.copy()
+random.shuffle(test_mod2_ix)
+test_mod1 = mod1[test_mod1_ix, :]
+test_mod2 = mod2[test_mod2_ix, :]
 
-print("Creating output objects")
+
+print("Creating mod1 outputs")
 desired_var1_cols = [x for x in ["gene_ids", "feature_types"] if x in mod1.var.columns]
-out_mod1 = anndata.AnnData(
-    X=mod1.X,
+out_train_mod1 = ad.AnnData(
+    X=train_mod1.X,
     var=mod1.var[desired_var1_cols],
     uns={ "dataset_id": new_dataset_id },
-    dtype="float32",
 )
-out_mod1.X.sort_indices()
+out_train_mod1.X.sort_indices()
+out_test_mod1 = ad.AnnData(
+    X=test_mod1.X,
+    var=mod1.var[desired_var1_cols],
+    uns={ "dataset_id": new_dataset_id },
+)
+out_test_mod1.X.sort_indices()
 
+print("Creating mod2 outputs")
 desired_var2_cols = [x for x in ["gene_ids", "feature_types"] if x in mod2.var.columns]
-out_mod2 = anndata.AnnData(
-    X=mod2.X[shuffle_cells, :],
+out_train_mod2 = ad.AnnData(
+    X=train_mod2.X,
     var=mod2.var[desired_var2_cols],
+    uns={ "dataset_id": new_dataset_id },
+)
+out_train_mod2.X.sort_indices()
+out_test_mod2 = ad.AnnData(
+    X=test_mod2.X,
+    var=mod2.var[desired_var2_cols],
+    uns={ "dataset_id": new_dataset_id },
+)
+out_test_mod2.X.sort_indices()
+
+print("Creating solution outputs")
+out_train_sol_mat = scipy.sparse.csr_matrix(
+  (np.ones(train_mod1_ix.size), (train_mod1_ix, train_mod2_ix))
+)
+out_train_sol = ad.AnnData(
+    X=out_train_sol_mat,
     uns={ "dataset_id": new_dataset_id },
     dtype="float32",
 )
-out_mod2.X.sort_indices()
-
-out_solution = anndata.AnnData(
-    X=pairings[:, shuffle_cells],
+out_test_sol_mat = scipy.sparse.csr_matrix(
+  (np.ones(test_mod1_ix.size), (test_mod1_ix, test_mod2_ix))
+)
+out_test_sol = ad.AnnData(
+    X=out_test_sol_mat,
     uns={ "dataset_id": new_dataset_id },
     dtype="float32"
 )
 
 print("Writing output objects to file")
-out_mod1.write_h5ad(filename=par["output_mod1"], compression="gzip")
-out_mod2.write_h5ad(filename=par["output_mod2"], compression="gzip")
-out_solution.write_h5ad(filename=par["output_solution"], compression="gzip")
+out_train_mod1.write_h5ad(filename=par["output_train_mod1"], compression="gzip")
+out_train_mod2.write_h5ad(filename=par["output_train_mod2"], compression="gzip")
+out_train_sol.write_h5ad(filename=par["output_train_sol"], compression="gzip")
+out_test_mod1.write_h5ad(filename=par["output_test_mod1"], compression="gzip")
+out_test_mod2.write_h5ad(filename=par["output_test_mod2"], compression="gzip")
+out_test_sol.write_h5ad(filename=par["output_test_sol"], compression="gzip")
