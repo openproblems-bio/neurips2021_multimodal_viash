71e514090f6390a9fbc8aa6a75e5a388b71f28bf
rcannood <rcannood@users.noreply.github.com>
2021-08-11 19:27:46 +0000

    deploy: 9bf7a41e61442bc9df05735575ddf66f20776cd4

--- src/predict_modality/methods/dummy_zeros/script.py
+++ src/predict_modality/methods/dummy_zeros/script.py
@@ -1,28 +1,29 @@
 import anndata
-from scipy.sparse import csr_matrix
+from scipy.sparse import csc_matrix
 import numpy as np
 
 # VIASH START
 par = {
-    "input_mod1": "resources_test/predict_modality/test_resource.mod1.h5ad",
-    "input_mod2": "resources_test/predict_modality/test_resource.mod2.h5ad",
-    "output": "test_resource.prediction.h5ad",
+    "input_train_mod1": "resources_test/predict_modality/test_resource.train_mod1.h5ad",
+    "input_test_mod1": "resources_test/predict_modality/test_resource.test_mod1.h5ad",
+    "input_train_mod2": "resources_test/predict_modality/test_resource.train_mod2.h5ad",
+    "output": "output.h5ad",
 }
 # VIASH END
 
 # load dataset to be censored
-ad_mod1 = anndata.read_h5ad(par["input_mod1"])
-ad_mod2 = anndata.read_h5ad(par["input_mod2"])
-ad_test = ad_mod1[ad_mod1.obs["group"] == "test"]
+ad_mod1_train = anndata.read_h5ad(par["input_train_mod1"])
+ad_mod1_test = anndata.read_h5ad(par["input_test_mod1"])
+ad_mod2 = anndata.read_h5ad(par["input_train_mod2"])
 
 # Testing with sparse prediction matrix
-prediction = csr_matrix((ad_test.n_obs, ad_mod2.n_vars), dtype = np.float32)
+prediction = csc_matrix((ad_mod1_test.n_obs, ad_mod2.n_vars), dtype = np.float32)
 
 # Write out prediction
 out = anndata.AnnData(
     X=prediction,
     uns={
-        "dataset_id": ad_mod1.uns["dataset_id"],
+        "dataset_id": ad_mod2.uns["dataset_id"],
         "method_id": "dummy_zeros",
     }
 )
