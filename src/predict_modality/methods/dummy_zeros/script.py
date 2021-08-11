4406527f0d5d5b6416da2765c2d0ed22285e542d
Louise Deconinck <louise.deconinck@gmail.com>
2021-08-11 18:33:06 +0200

    Finish refactor of task 1

--- src/predict_modality/methods/dummy_zeros/script.py
+++ src/predict_modality/methods/dummy_zeros/script.py
@@ -1,5 +1,5 @@
 import anndata
-from scipy.sparse import csr_matrix
+from scipy.sparse import csc_matrix
 import numpy as np
 
 # VIASH START
@@ -17,7 +17,7 @@ ad_mod1_test = anndata.read_h5ad(par["input_test_mod1"])
 ad_mod2 = anndata.read_h5ad(par["input_train_mod2"])
 
 # Testing with sparse prediction matrix
-prediction = csr_matrix((ad_mod1_test.n_obs, ad_mod2.n_vars), dtype = np.float32)
+prediction = csc_matrix((ad_mod1_test.n_obs, ad_mod2.n_vars), dtype = np.float32)
 
 # Write out prediction
 out = anndata.AnnData(
