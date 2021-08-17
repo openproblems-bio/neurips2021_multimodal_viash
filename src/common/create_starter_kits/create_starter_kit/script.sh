090b079d65ee16f7f29dd7b953fb105a3a198335
rcannood <rcannood@users.noreply.github.com>
2021-08-17 09:21:44 +0000

    deploy: 77d405e05296af0699b2fbb4a8ef9adfc985dc5f

--- src/common/create_starter_kits/create_starter_kit/script.sh
+++ src/common/create_starter_kits/create_starter_kit/script.sh
@@ -3,7 +3,7 @@
 set -e
 
 ## VIASH START
-par_src=src/
+par_input_dir=src/predict_modality/starter_kit/starter_kit_r/
 par_output_dir=output/starter_kits
 par_task=predict_modality
 par_language=r
@@ -17,25 +17,22 @@ par_language_name=R
 resources_dir=src/common/create_starter_kits/create_starter_kit/
 ## VIASH END
 
-input_dir="$par_src/$par_task/starter_kit/starter_kit_$par_language"
 output_dir="$par_output_dir/starter_kit-$par_task-$par_language"
 
-echo ">> Creating $par_task_name starter kit for $par_language_name users."
-
-echo "  Remove previous results"
+echo remove previous results
 [[ -d $output_dir ]] && rm -r $output_dir
 
-echo "  Create new output dir"
+echo create new output dir
 mkdir -p $output_dir
 
-echo "  Copy template files"
+echo copy template files
 cp $resources_dir/template_files/README.md $output_dir/
 cp $resources_dir/template_files/generate_submission.sh $output_dir/
 cp $resources_dir/template_files/nextflow.config $output_dir/
 cp $resources_dir/template_files/LICENSE $output_dir/
 cp $resources_dir/template_files/.gitignore $output_dir/
 
-echo "  Replace terms in templates"
+echo replace terms in templates
 sed -i "s#\\\$par_task_name#$par_task_name#g" $output_dir/*
 sed -i "s#\\\$par_task#$par_task#g" $output_dir/*
 sed -i "s#\\\$par_language_name#$par_language_name#g" $output_dir/*
@@ -47,23 +44,20 @@ sed -i "s#\\\$par_cpus#$par_cpus#g" $output_dir/*
 sed -i "s#\\\$par_pipeline_version#$par_pipeline_version#g" $output_dir/*
 sed -i "s#\\\$par_block_starter#$par_block_starter#g" $output_dir/*
 
-echo "  Run viash dockerfile"
-dockerfile=$(viash run $input_dir/config.vsh.yaml -- ---dockerfile | sed 's#^#\t#' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's#&#\\\&#g')
+echo run viash dockerfile
+dockerfile=$(viash run $par_input_dir/config.vsh.yaml -- ---dockerfile | sed 's#^#\t#' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's#&#\\\&#g')
 sed -i "s~\\\$codeblock_dockerfile~$dockerfile~g" $output_dir/*
 
-echo "  Copy executables"
+echo copy executables
 mkdir $output_dir/bin
 cp $par_bin/viash $output_dir/bin/
 cp $par_bin/nextflow $output_dir/bin/
 
-echo "  Copy scripts"
-cp $input_dir/* $output_dir
-
-echo "  Build check_format component"
-viash build $src/$par_task/metrics/check_format/config.vsh.yaml -p docker -o $output_dir/bin/
+echo copy scripts
+cp $par_input_dir/* $output_dir
 
 # todo: update to multisample
-echo "  Copy sample resources"
+echo copy sample resources
 mkdir -p $output_dir/sample_data/
 if [[ $par_task == "predict_modality" ]]; then
   cp $resources_dir/resources_test/$par_task/test_resource.train_mod[12].h5ad $output_dir/sample_data/
@@ -75,8 +69,8 @@ elif [[ $par_task == "joint_embedding" ]]; then
   cp $resources_dir/resources_test/$par_task/test_resource.mod[12].h5ad $output_dir/sample_data/
 fi
 
-echo "  Zipping starter kit"
+echo zipping starter kit
 [ -f ${output_dir}.zip ] && rm ${output_dir}.zip
 cd  ${output_dir} && zip -9 -r ../$(basename $output_dir).zip *
 
-echo "  Starter kit is done!"
+echo starter kit is done!
