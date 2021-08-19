cf0f2226b38a073bc62d9ff0954ce2cb2fdff4f8
Robrecht Cannoodt <rcannood@gmail.com>
2021-08-19 09:51:55 +0200

    Metric: Added MSE and MSLE metrics.

--- src/predict_modality/metrics/msle/script.R
+++ src/predict_modality/metrics/msle/script.R
@@ -29,30 +29,30 @@ expect_true(
   info = "Dataset and prediction anndata objects should have the same shape / dimensions."
 )
 
-cat("Computing correlation metrics\n")
+cat("Computing MSE and MSLE metrics\n")
 # Wrangle data
 tv <- ad_sol$X
 pv <- ad_pred$X
 
-# Compute metrics
-pearson_mat <- dynutils::calculate_similarity(tv, pv, method = "pearson", margin = 2, diag = TRUE, drop0 = TRUE)
-spearman_mat <- dynutils::calculate_similarity(tv, pv, method = "spearman", margin = 2, diag = TRUE, drop0 = TRUE)
+# compute logs
+ltv <- tv
+ltv@x <- log(ltv@x + 1)
 
-mean_pearson <- mean(diag(pearson_mat))
-mean_spearman <- mean(diag(spearman_mat))
-score_mean_pearson <- mean_pearson / 2 + .5
-score_mean_spearman <- mean_spearman / 2 + .5
+# take into account there might be negative values in there
+lpv <- pv
+lpv@x <- log(abs(lpv@x) + 1) * sign(lpv@x)
 
-metric_ids <- c("mean_pearson", "mean_spearman", "score_mean_pearson", "score_mean_spearman")
-metric_values <- c(mean_pearson, mean_spearman, score_mean_pearson, score_mean_spearman)
+# compute metrics
+mse <- mean((tv - pv)^2)
+msle <- mean((ltv - lpv)^2)
 
 cat("Create output object\n")
 out <- anndata::AnnData(
   uns = list(
     dataset_id = ad_pred$uns$dataset_id,
     method_id = ad_pred$uns$method_id,
-    metric_ids = metric_ids,
-    metric_values = metric_values
+    metric_ids = c("mse", "msle"),
+    metric_values = c(mse, msle)
   )
 )
 
