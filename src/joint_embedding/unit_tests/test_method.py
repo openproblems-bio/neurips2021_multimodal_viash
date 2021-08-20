070dd92b6f5a207cd0c8ab159ae7a0699af33d08
Robrecht Cannoodt <rcannood@gmail.com>
2021-08-20 10:05:13 +0200

    refactor unit tests

--- src/joint_embedding/unit_tests/test_method.py
+++ src/joint_embedding/unit_tests/test_method.py
@@ -2,7 +2,16 @@ from os import path
 import subprocess
 import anndata as ad
 
-par = {
+## VIASH START
+# This code block will be replaced by viash at runtime.
+meta = { 'functionality_name': 'foo' }
+## VIASH END
+
+method_id = meta['functionality_name']
+command = "./" + method_id
+
+# define some filenames
+testpar = {
     "input_mod1": "resources_test/joint_embedding/test_resource.mod1.h5ad",
     "input_mod2": "resources_test/joint_embedding/test_resource.mod2.h5ad",
     "output": "output.h5ad",
@@ -10,22 +19,22 @@ par = {
 
 print("> Running method")
 out = subprocess.check_output([
-  "./" + meta['functionality_name'],
-  "--input_mod1", par['input_mod1'],
-  "--input_mod2", par['input_mod2'],
-  "--output", par['output']
+  command,
+  "--input_mod1", testpar['input_mod1'],
+  "--input_mod2", testpar['input_mod2'],
+  "--output", testpar['output']
 ]).decode("utf-8")
 
 print("> Checking whether output files were created")
-assert path.exists(par['output'])
+assert path.exists(testpar['output'])
 
 print("> Reading h5ad files")
-input_mod1 = ad.read_h5ad(par['input_mod1'])
-output = ad.read_h5ad(par['output'])
+input_mod1 = ad.read_h5ad(testpar['input_mod1'])
+output = ad.read_h5ad(testpar['output'])
 
 print("> Checking contents of output.h5ad")
 assert output.uns['dataset_id'] == input_mod1.uns['dataset_id']
-assert output.uns['method_id'] == meta['functionality_name']
+assert output.uns['method_id'] == method_id
 assert output.n_obs == input_mod1.n_obs
 assert output.n_vars >= 1
 assert output.n_vars <= 100
