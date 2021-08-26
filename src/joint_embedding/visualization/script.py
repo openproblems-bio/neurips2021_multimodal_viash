a9ba43830e2a58c0914a5b2f351573df6fa49ea8
Michaela Mueller <mumichae@in.tum.de>
2021-08-26 10:52:58 +0200

    include plot on embedding directly

--- src/joint_embedding/visualization/script.py
+++ src/joint_embedding/visualization/script.py
@@ -13,7 +13,8 @@ logging.basicConfig(level=logging.INFO)
 par = {
     'input_prediction': 'resources_test/joint_embedding/test_resource.prediction.h5ad',
     'input_solution': 'resources_test/joint_embedding/test_resource.solution.h5ad',
-    'output': 'resources_test/joint_embedding/test_resource.umap.png'
+    'output_umap': 'resources_test/joint_embedding/test_resource.umap.png',
+    'output_emb': 'resources_test/joint_embedding/test_resource.emb.png'
 }
 
 ## VIASH END
@@ -34,5 +35,17 @@ logging.info('UMAP...')
 sc.tl.umap(adata)
 
 logging.info('Plotting...')
+# UMAP
 sc.pl.umap(adata, color=['batch', 'cell_type'], show=False)
-plt.savefig(par['output'])
+plt.savefig(par['output_umap'])
+
+# Embedding
+sc.pl.embedding(adata, basis='X_emb', color=['batch', 'cell_type'])
+plt.savefig(par['output_emb'])
+
+# adata.obs['Emb_1'] = adata.X[:, 1]
+# adata.obs['Emb_2'] = adata.X[:, 2]
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+#
+# sc.pl.scatter(adata, x='Emb_1', y='Emb_2', color='batch', show=False, ax=ax1)
+# sc.pl.scatter(adata, x='Emb_1', y='Emb_2', color='cell_type', show=False, ax=ax2)
