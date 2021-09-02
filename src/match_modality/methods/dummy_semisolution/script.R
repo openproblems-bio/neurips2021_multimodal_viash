6838053f5ab250b05f34cb4102e6499813f58da1
rcannood <rcannood@users.noreply.github.com>
2021-09-02 15:53:08 +0000

    deploy: 219119e93ad8c7d0f4cfc7e47b1b4aae145ffb1a

--- src/match_modality/methods/dummy_semisolution/script.R
+++ src/match_modality/methods/dummy_semisolution/script.R
@@ -3,7 +3,7 @@ requireNamespace("anndata", quietly = TRUE)
 
 ## VIASH START
 par <- list(
-  input_test_sol = "resources_test/match_modality/test_resource.train_sol.h5ad",
+  input_test_sol = "resources_test/match_modality/test_resource.test_sol.h5ad",
   output = "output.h5ad"
 )
 meta <- list(functionality_name = "foo")
@@ -11,6 +11,13 @@ meta <- list(functionality_name = "foo")
 
 cat("Reading h5ad files\n")
 input_test_sol <- anndata::read_h5ad(par$input_test_sol)
+
+# randomly fill in gold standard values
+input_test_sol$X@x <- runif(length(input_test_sol$X@x))
+
+# fill other values with random values as well
+ix <- sample.int(nrow(input_test_sol) * ncol(input_test_sol), nrow(input_test_sol) * 10)
+input_test_sol$X[ix] <- runif(length(ix))
 
 input_test_sol$uns[["method_id"]] <- meta$functionality_name
 
