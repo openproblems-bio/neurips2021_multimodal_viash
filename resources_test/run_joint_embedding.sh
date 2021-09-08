c0b17f0e8871c34b80a7dd1d06b8cc97e4d0bcf5
Robrecht Cannoodt <rcannood@gmail.com>
2021-09-08 13:06:11 +0200

    update resources test

--- resources_test/run_joint_embedding.sh
+++ resources_test/run_joint_embedding.sh
@@ -11,9 +11,8 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-target_dir=target/docker
-in_file=resources_test/common/test_resource
-out_file=resources_test/joint_embedding/test_resource
+in_file=resources_test/common/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter
+out_file=resources_test/joint_embedding/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter
 
 # remove previous output
 out_dir=`dirname $out_file`
@@ -40,5 +39,37 @@ viash run src/joint_embedding/metrics/rfoob/config.vsh.yaml -- \
 viash run src/common/extract_scores/config.vsh.yaml -- \
   --input ${out_file}.scores.h5ad \
   --metric_meta src/joint_embedding/metrics/rfoob/metric_meta_rfoob.tsv \
-  --output ${out_file}.scores.tsv \
-  --summary ${out_file}.summary.tsv
+  --output ${out_file}.scores.tsv
+
+
+
+
+in_file=resources_test/common/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter
+out_file=resources_test/joint_embedding/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter
+
+# remove previous output
+out_dir=`dirname $out_file`
+[ -d $out_dir ] && rm -r $out_dir
+mkdir -p $out_dir
+
+viash run src/joint_embedding/datasets/censor_dataset/config.vsh.yaml -- \
+  --input_mod1 ${in_file}.output_rna.h5ad \
+  --input_mod2 ${in_file}.output_mod2.h5ad \
+  --output_mod1 ${out_file}.mod1.h5ad \
+  --output_mod2 ${out_file}.mod2.h5ad \
+  --output_solution ${out_file}.solution.h5ad
+  
+viash run src/joint_embedding/methods/baseline_lmds/config.vsh.yaml -- \
+  --input_mod1 ${out_file}.mod1.h5ad \
+  --input_mod2 ${out_file}.mod2.h5ad \
+  --output ${out_file}.prediction.h5ad
+  
+viash run src/joint_embedding/metrics/rfoob/config.vsh.yaml -- \
+  --input_prediction ${out_file}.prediction.h5ad \
+  --input_solution ${out_file}.solution.h5ad \
+  --output ${out_file}.scores.h5ad
+
+viash run src/common/extract_scores/config.vsh.yaml -- \
+  --input ${out_file}.scores.h5ad \
+  --metric_meta src/joint_embedding/metrics/rfoob/metric_meta_rfoob.tsv \
+  --output ${out_file}.scores.tsv
--- src/common/datasets/pilot_data.R
+++ src/common/datasets/pilot_data.R
@@ -7,10 +7,12 @@ set.seed(1)
 # sync to local folder
 # system("aws s3 sync s3://openproblems-bio/public/explore/ output/manual_formatting/ --profile op2")
 
+starter_dev <- c("s1d1", "s1d2")
 train <- c("s1d1", "s2d1", "s2d4", "s3d6", "s3d1")
 valid <- c("s1d2", "s3d7")
 backup_test <- c("s1d3", "s2d5")
 
+resources_test <- "resources_test/common/"
 output_dir <- "output/datasets/common/"
 
 #############################
@@ -19,8 +21,9 @@ output_dir <- "output/datasets/common/"
 # read data
 ad1 <- read_h5ad("output/manual_formatting/multiome/Multiome_GEX_processed.training.h5ad")
 ad2 <- read_h5ad("output/manual_formatting/multiome/Multiome_ATAC_processed.training.h5ad")
-dataset_id <- ad1$uns[["dataset_id"]]
+adid <- ad1$uns[["dataset_id"]]
 
+unique(ad1$obs$batch)
 testthat::expect_length(setdiff(ad1$obs$batch, c(train, valid, backup_test)), 0)
 setdiff(c(train, valid, backup_test), unique(ad1$obs$batch))
 
@@ -54,16 +57,13 @@ ad2_phase1 <- ad2[ad2$obs$batch %in% c(train, valid), ]$copy()
 ad1_phase1$obs[["is_train"]] <- ad1_phase1$obs[["batch"]] %in% train
 ad2_phase1$obs[["is_train"]] <- ad1_phase1$obs[["batch"]] %in% train
 
-did_phase1 <- paste0(dataset_id, "_phase1")
-ad1_phase1$uns[["dataset_id"]] <- did_phase1
-ad2_phase1$uns[["dataset_id"]] <- did_phase1
+adid_phase1 <- paste0(adid, "_phase1")
+ad1_phase1$uns[["dataset_id"]] <- adid_phase1
+ad2_phase1$uns[["dataset_id"]] <- adid_phase1
 
-ad1_phase1$X
-ad2_phase1$X
-
-dir.create(paste0(output_dir, did_phase1), recursive = TRUE)
-ad1_phase1$write_h5ad(paste0(output_dir, did_phase1, "/", did_phase1, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
-ad2_phase1$write_h5ad(paste0(output_dir, did_phase1, "/", did_phase1, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
+dir.create(paste0(output_dir, adid_phase1), recursive = TRUE)
+ad1_phase1$write_h5ad(paste0(output_dir, adid_phase1, "/", adid_phase1, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
+ad2_phase1$write_h5ad(paste0(output_dir, adid_phase1, "/", adid_phase1, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
 
 
 #############
@@ -75,13 +75,39 @@ ad2_phase2 <- ad2$copy()
 ad1_phase2$obs[["is_train"]] <- ad1_phase2$obs[["batch"]] %in% c(train, valid)
 ad2_phase2$obs[["is_train"]] <- ad1_phase2$obs[["batch"]] %in% c(train, valid)
 
-did_phase2 <- paste0(dataset_id, "_phase2")
-ad1_phase2$uns[["dataset_id"]] <- did_phase2
-ad2_phase2$uns[["dataset_id"]] <- did_phase2
+adid_phase2 <- paste0(adid, "_phase2")
+ad1_phase2$uns[["dataset_id"]] <- adid_phase2
+ad2_phase2$uns[["dataset_id"]] <- adid_phase2
 
-dir.create(paste0(output_dir, did_phase2), recursive = TRUE)
-ad1_phase2$write_h5ad(paste0(output_dir, did_phase2, "/", did_phase2, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
-ad2_phase2$write_h5ad(paste0(output_dir, did_phase2, "/", did_phase2, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
+dir.create(paste0(output_dir, adid_phase2), recursive = TRUE)
+ad1_phase2$write_h5ad(paste0(output_dir, adid_phase2, "/", adid_phase2, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
+ad2_phase2$write_h5ad(paste0(output_dir, adid_phase2, "/", adid_phase2, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
+
+
+#############
+# create test_resource
+
+starter_cell <- which(ad1$obs$batch %in% starter_dev)
+row_sel <- sort(sample(starter_cell, 500))
+col_sel1 <- seq_len(ncol(ad1)) %in% sample.int(ncol(ad1), 600, prob = Matrix::colSums(ad1$X > 0))
+col_sel2 <- seq_len(ncol(ad2)) %in% sample.int(ncol(ad2), 600, prob = Matrix::colSums(ad2$X > 0))
+
+ad1_starter <- ad1[row_sel, col_sel1]$copy()
+ad2_starter <- ad2[row_sel, col_sel2]$copy()
+
+ad1_starter$obsm <- NULL
+ad2_starter$obsm <- NULL
+
+ad1_starter$obs[["is_train"]] <- ad1_starter$obs[["batch"]] %in% train
+ad2_starter$obs[["is_train"]] <- ad2_starter$obs[["batch"]] %in% train
+
+adid_starter <- paste0(adid, "_starter")
+ad1_starter$uns[["dataset_id"]] <- adid_starter
+ad2_starter$uns[["dataset_id"]] <- adid_starter
+
+dir.create(paste0(resources_test, adid_starter), recursive = TRUE)
+ad1_starter$write_h5ad(paste0(resources_test, adid_starter, "/", adid_starter, ".output_rna.h5ad"), compression = "gzip")
+ad2_starter$write_h5ad(paste0(resources_test, adid_starter, "/", adid_starter, ".output_mod2.h5ad"), compression = "gzip")
 
 
 
@@ -93,7 +119,7 @@ ad2_phase2$write_h5ad(paste0(output_dir, did_phase2, "/", did_phase2, ".manual_f
 # read data
 bd1 <- read_h5ad("output/manual_formatting/cite/CITE_GEX_processed.training.h5ad")
 bd2 <- read_h5ad("output/manual_formatting/cite/CITE_ADT_processed.training.h5ad")
-dataset_id <- bd1$uns[["dataset_id"]]
+bdid <- bd1$uns[["dataset_id"]]
 
 testthat::expect_length(setdiff(bd1$obs$batch, c(train, valid, backup_test)), 0)
 setdiff(c(train, valid, backup_test), unique(bd1$obs$batch))
@@ -129,13 +155,13 @@ bd2_phase1 <- bd2[bd2$obs$batch %in% c(train, valid), ]$copy()
 bd1_phase1$obs[["is_train"]] <- bd1_phase1$obs[["batch"]] %in% train
 bd2_phase1$obs[["is_train"]] <- bd1_phase1$obs[["batch"]] %in% train
 
-did_phase1 <- paste0(dataset_id, "_phase1")
-bd1_phase1$uns[["dataset_id"]] <- did_phase1
-bd2_phase1$uns[["dataset_id"]] <- did_phase1
+bdid_phase1 <- paste0(bdid, "_phase1")
+bd1_phase1$uns[["dataset_id"]] <- bdid_phase1
+bd2_phase1$uns[["dataset_id"]] <- bdid_phase1
 
-dir.create(paste0(output_dir, did_phase1), recursive = TRUE)
-bd1_phase1$write_h5ad(paste0(output_dir, did_phase1, "/", did_phase1, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
-bd2_phase1$write_h5ad(paste0(output_dir, did_phase1, "/", did_phase1, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
+dir.create(paste0(output_dir, bdid_phase1), recursive = TRUE)
+bd1_phase1$write_h5ad(paste0(output_dir, bdid_phase1, "/", bdid_phase1, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
+bd2_phase1$write_h5ad(paste0(output_dir, bdid_phase1, "/", bdid_phase1, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
 
 
 # #############
@@ -147,11 +173,36 @@ bd2_phase1$write_h5ad(paste0(output_dir, did_phase1, "/", did_phase1, ".manual_f
 # bd1_phase2$obs[["is_train"]] <- bd1_phase2$obs[["batch"]] %in% c(train, valid)
 # bd2_phase2$obs[["is_train"]] <- bd1_phase2$obs[["batch"]] %in% c(train, valid)
 
-# did_phase2 <- paste0(dataset_id, "_phase2")
-# bd1_phase2$uns[["dataset_id"]] <- did_phase2
-# bd2_phase2$uns[["dataset_id"]] <- did_phase2
+# bdid_phase2 <- paste0(bdid, "_phase2")
+# bd1_phase2$uns[["dataset_id"]] <- bdid_phase2
+# bd2_phase2$uns[["dataset_id"]] <- bdid_phase2
 
-# dir.create(paste0(output_dir, did_phase2), recursive = TRUE)
-# bd1_phase2$write_h5ad(paste0(output_dir, did_phase2, "/", did_phase2, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
-# bd2_phase2$write_h5ad(paste0(output_dir, did_phase2, "/", did_phase2, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
+# dir.create(paste0(output_dir, bdid_phase2), recursive = TRUE)
+# bd1_phase2$write_h5ad(paste0(output_dir, bdid_phase2, "/", bdid_phase2, ".manual_formatting.output_rna.h5ad"), compression = "gzip")
+# bd2_phase2$write_h5ad(paste0(output_dir, bdid_phase2, "/", bdid_phase2, ".manual_formatting.output_mod2.h5ad"), compression = "gzip")
 
+
+
+#############
+# create test_resource
+
+starter_cell <- which(bd1$obs$batch %in% starter_dev)
+row_sel <- sort(sample(starter_cell, 600))
+col_sel1 <- seq_len(ncol(bd1)) %in% sample.int(ncol(bd1), 1000, prob = Matrix::colSums(bd1$X > 0))
+
+bd1_starter <- bd1[row_sel, col_sel1]$copy()
+bd2_starter <- bd2[row_sel, ]$copy()
+
+bd1_starter$obs[["is_train"]] <- bd1_starter$obs[["batch"]] %in% train
+bd2_starter$obs[["is_train"]] <- bd2_starter$obs[["batch"]] %in% train
+
+bdid_starter <- paste0(bdid, "_starter")
+bd1_starter$uns[["dataset_id"]] <- bdid_starter
+bd2_starter$uns[["dataset_id"]] <- bdid_starter
+
+bd1_starter$obsm <- NULL
+bd2_starter$obsm <- NULL
+
+dir.create(paste0(resources_test, bdid_starter), recursive = TRUE)
+bd1_starter$write_h5ad(paste0(resources_test, bdid_starter, "/", bdid_starter, ".output_rna.h5ad"), compression = "gzip")
+bd2_starter$write_h5ad(paste0(resources_test, bdid_starter, "/", bdid_starter, ".output_mod2.h5ad"), compression = "gzip")
