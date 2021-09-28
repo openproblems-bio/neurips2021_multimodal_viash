29e8d61689487f0362a3cea5ffe704c2b8b1e6de
Robrecht Cannoodt <rcannood@gmail.com>
2021-09-28 08:49:55 +0200

    optimise baseline methods

--- src/joint_embedding/methods/baseline_lmds/script.R
+++ src/joint_embedding/methods/baseline_lmds/script.R
@@ -5,9 +5,11 @@ requireNamespace("anndata", quietly = TRUE)
 library(Matrix, warn.conflicts = FALSE, quietly = TRUE)
 
 ## VIASH START
+# path <- "resources_test/joint_embedding/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter."
+path <- "output/datasets/joint_embedding/openproblems_bmmc_multiome_phase1/openproblems_bmmc_multiome_phase1.censor_dataset.output_"
 par <- list(
-  input_mod1 = "resources_test/joint_embedding/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.mod1.h5ad",
-  input_mod2 = "resources_test/joint_embedding/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.mod2.h5ad",
+  input_mod1 = paste0(path, "mod1.h5ad"),
+  input_mod2 = paste0(path, "mod2.h5ad"),
   output = "output.h5ad",
   n_dims = 10L,
   distance_method = "spearman"
